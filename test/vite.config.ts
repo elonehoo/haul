8cb56f2954edabe4218f4acccd325c36fa4bb7b1
xiaoxunyao <huchengyea@163.com>
2022-06-30 17:45:07 +0800

    config: init vite config ts

---
+++ test/vite.config.ts
@@ -0,0 +1,15 @@
+import { UserConfigExport, ConfigEnv } from 'vite'
+
+import { viteMockServe } from 'vite-plugin-mock'
+
+export default ({ command }: ConfigEnv): UserConfigExport => {
+  return {
+    plugins: [
+      viteMockServe({
+        // default
+        mockPath: 'mock',
+        localEnabled: command === 'serve',
+      }),
+    ],
+  }
+}
