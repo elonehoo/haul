7e4eada066d043fdc39f7e6c065cc9f386680078
xiaoxunyao <huchengyea@163.com>
2022-06-30 09:21:02 +0800

    fix: global default options

--- core/src/index.ts
+++ core/src/index.ts
@@ -1,4 +1,21 @@
 import {ResponseAsTypes,Options,OptionsRaw,HaulError,HaulInstance} from './type'
 
+/**
+ * Global default options as {@link Options} that are applied to **all** haul
+ * instances. Always contain an initialized `headers` property with the default
+ * headers:
+ * - Accept: 'application/json'
+ * - 'Content-Type': 'application/json'
+ */
+export const defaults: Options &
+  Pick<Required<Options>, 'headers' | 'responseAs'> = {
+  responseAs: 'json',
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  },
+}
+
+
 
 
