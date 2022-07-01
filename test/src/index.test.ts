9d7b3fda08efa2267e3553a3da34e90e398c450d
xiaoxunyao <huchengyea@163.com>
2022-07-01 09:36:29 +0800

    fix: update test

--- test/src/index.test.ts
+++ test/src/index.test.ts
@@ -1,10 +1,17 @@
-import {expect, test } from 'vitest'
+import { test } from 'vitest'
 
 import {haul} from '@elonehoo/haul'
+import fetch from 'node-fetch'
+import Global = NodeJS.Global;
+export interface GlobalWithCognitoFix extends Global {
+    fetch: any
+}
+declare const global: GlobalWithCognitoFix;
+global.fetch = fetch
 
 test('test in mock',()=>{
-  const base = haul('http://example.com/api/')
-  base.get('').then((res)=>{
+  const hi = haul('http://localhost:8080/item/hi')
+  hi.get('/').then(res=>{
     console.log(res)
   })
 })
